3432a220e4312ffc057463a4f19baf646f9a5f86
lewis-developer-33 <shadowvictor33@gmail.com>
2025-04-02 21:45:10 +0300

    deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,22 @@ import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro'
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-      sanity({
-        projectId: 'l0foa61x',
-        dataset: 'production',        
-        useCdn: false,
-        studioBasePath:'/studio',
-        stega:{
-          studioUrl:'/studio'
-        }
-      }), 
-      react()
-    ],
-  })
+  integrations: [
+    sanity({
+      projectId: 'l0foa61x',
+      dataset: 'production',        
+      useCdn: false,
+      studioBasePath:'/studio',
+      stega:{
+        studioUrl:'/studio'
+      }
+    }), 
+    react()
+  ],
+  output:'server',
+  adapter: netlify()
+})
